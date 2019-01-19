5c560e8cb40336efdf56efb9ab2577a9c154aad6
Thomas Nguyen <hi@thomasnguyen.co>
2019-01-19 11:30:38 -0800

    Configured angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'books', component: BooksComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
