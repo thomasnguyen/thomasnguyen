34549fd42bb2417d65237b4097b2d57c7e6906fc
Thomas Nguyen <hi@thomasnguyen.co>
2019-01-19 11:08:14 -0800

    Initializd components through angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { BooksComponent } from './books/books.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    WorkComponent,
+    BooksComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
